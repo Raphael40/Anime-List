b01526569927fe112ce7e28c14b9b63f4f5543f3
Julian Festing <julfesting@gmail.com>
2024-03-12 14:17:40 +0000

    Header component links

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,18 @@
+import { NavLink } from 'react-router-dom';
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<main>
+			<header>
+				<nav>
+					<NavLink to='/'>Home</NavLink>
+					<NavLink to='/anime'>Anime</NavLink>
+					<NavLink to='/genres'>Genres</NavLink>
+					<NavLink to='/search'>Search</NavLink>
+				</nav>
+			</header>
+		</main>
+	);
 };
 
 export default Header;
