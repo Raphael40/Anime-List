7b26a42d928b7f31198f19b73394594bf9bd5fb7
Julian Festing <julfesting@gmail.com>
2024-03-20 13:38:49 +0000

    pass Genres component tests with 100% coverage

--- src/components/Genres/Genres.test.jsx
+++ src/components/Genres/Genres.test.jsx
@@ -1,16 +1,18 @@
-import { beforeEach, afterEach, describe, it, expect } from 'vitest';
+import { beforeEach, afterEach, describe, it, expect, vi } from 'vitest';
 import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import * as matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
 
 import Genres from '.';
 
+const setGenreMock = vi.fn();
 const genres = [{ _id: 'Test Genre' }, { _id: 'Second Test Genre' }];
 
 describe('Genres component', () => {
 	beforeEach(() => {
-		render(<Genres genres={genres} />);
+		render(<Genres genres={genres} setGenre={setGenreMock} />);
 	});
 
 	afterEach(() => {
@@ -24,11 +26,25 @@ describe('Genres component', () => {
 		expect(genresList.childNodes.length).toBe(3);
 	});
 
-	it('Displays two p tags containing the text, Test Genre and Second Test Genre', async () => {
+	it('Displays two p tags containing the text; Test Genre and Second Test Genre', async () => {
 		const testGenre = screen.getByText('Test Genre');
 		const secondTestGenre = screen.getByText('Second Test Genre');
 
 		expect(testGenre).toBeInTheDocument();
 		expect(secondTestGenre).toBeInTheDocument();
+	});
+
+	it('Calls setGenre with correct genre when a genre tag is clicked', async () => {
+		const testGenre = screen.getByText('Test Genre');
+		await userEvent.click(testGenre);
+
+		expect(setGenreMock).toHaveBeenCalledWith('Test Genre');
+	});
+
+	it('Calls setGenre with empty string when "All" tag is clicked', async () => {
+		const allGenreTag = screen.getByText('All');
+		await userEvent.click(allGenreTag);
+
+		expect(setGenreMock).toHaveBeenCalledWith('');
 	});
 });
